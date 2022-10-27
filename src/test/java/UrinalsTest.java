529151ccfcfce33ae3513e45d41601115ac98d89
VidhinASU <vidhin@asu.edu>
2022-10-27 16:44:31 -0700

    Add a JUnit Test string with alphabets

--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -3,10 +3,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UrinalsTest {
+    // Test cases
+
+    // good case
     @Test
     void goodString() {
         System.out.println("====== Vidhin Parmar== TEST ONE EXECUTED =======");
         Urinals urinals = new Urinals();
         assertTrue(urinals.goodString("1011"));
+    }
+    //bad alphabet string
+    @Test
+    void badString() {
+        System.out.println("====== Vidhin Parmar== TEST TWO EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertFalse(urinals.goodString("101a"));
     }
 }
