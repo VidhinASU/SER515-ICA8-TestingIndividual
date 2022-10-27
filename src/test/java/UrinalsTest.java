0edd79808decb2d5996ed7e1a43c094189016232
VidhinASU <vidhin@asu.edu>
2022-10-27 16:45:33 -0700

    Add a JUnit Test string with non boolean numbers

--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -5,18 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class UrinalsTest {
     // Test cases
 
-    // good case
+    // Good case
     @Test
     void goodString() {
         System.out.println("====== Vidhin Parmar== TEST ONE EXECUTED =======");
         Urinals urinals = new Urinals();
         assertTrue(urinals.goodString("1011"));
     }
-    //bad alphabet string
+    // Bad alphabet string
     @Test
     void badString() {
         System.out.println("====== Vidhin Parmar== TEST TWO EXECUTED =======");
         Urinals urinals = new Urinals();
         assertFalse(urinals.goodString("101a"));
+    }
+    
+    // Bad number string
+    @Test
+    void badString2() {
+        System.out.println("====== Vidhin Parmar== TEST THREE EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertFalse(urinals.goodString("1012"));
     }
 }
