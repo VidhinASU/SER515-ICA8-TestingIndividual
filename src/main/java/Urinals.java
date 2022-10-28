a8c3d08f81e819bfd1c6eb94ba317b7beaa4b3ea
VidhinASU <vidhin@asu.edu>
2022-10-27 17:21:09 -0700

    Add method goodPublicUrinalString and getFreeUrinals along with their 10 test cases

--- src/main/java/Urinals.java
+++ src/main/java/Urinals.java
@@ -2,13 +2,13 @@
  * This is a class for Urinals
  *
  * @author Vidhin
- *
  */
 public class Urinals {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
-    public Boolean goodString( String str ) {
+
+    public Boolean goodString(String str) {
         // System.out.println ("Not yet implemented");
         // checks to see if valid string
         for (int i = 0; i < str.length(); i++) {
@@ -17,6 +17,60 @@ public class Urinals {
             }
         }
 
-        return true; 
-   }
+        return true;
+    }
+
+    public Boolean goodPublicUrinalString(String str) {
+        // System.out.println ("Not yet implemented");
+        // checks to see if valid string using goodString method
+        if (goodString(str) == false) {
+            return false;
+        }
+
+        // checks to see if there are 2 1's in a row
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '1' && str.charAt(i + 1) == '1') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // given a string of urinals, return the number of urinals that can be used as
+    // mens
+    // public toilet, there is this unwritten rule that you leave at least one
+    // urinal
+    // free between you and the next person peeing
+    public int getFreeUrinals(String str) {
+        // System.out.println ("Not yet implemented");
+        int count = 0;
+        // if the string is not valid or a valid unwritten rule satisfying string,
+        // return -1
+        if (!goodPublicUrinalString(str)) {
+            return -1;
+        }
+
+        // counts the number of free urinals
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '0') {
+                if (i == 0) {
+                    if (str.charAt(i + 1) == '0') {
+                        count++;
+                        i++;
+                    }
+                } else if (i == str.length() - 1) {
+                    if (str.charAt(i - 1) == '0') {
+                        count++;
+                    }
+                } else {
+                    if (str.charAt(i - 1) == '0' && str.charAt(i + 1) == '0') {
+                        count++;
+                        i++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -12,6 +12,7 @@ class UrinalsTest {
         Urinals urinals = new Urinals();
         assertTrue(urinals.goodString("1011"));
     }
+
     // Bad alphabet string
     @Test
     void badString() {
@@ -19,12 +20,106 @@ class UrinalsTest {
         Urinals urinals = new Urinals();
         assertFalse(urinals.goodString("101a"));
     }
-    
+
     // Bad number string
     @Test
     void badString2() {
         System.out.println("====== Vidhin Parmar== TEST THREE EXECUTED =======");
         Urinals urinals = new Urinals();
         assertFalse(urinals.goodString("1012"));
+    }
+
+    // 10001 returns 1 (10101)
+    // 1001 returns 0 (1001)
+    // 00000 returns 3 (10101)
+    // 0000 returns 2 (1001)
+    // 01000 returns 1 (01010 or 01001)
+    // 011 returns -1
+    // Count max free urinals string 1
+    @Test
+    void getFreeUrinals() {
+        System.out.println("====== Vidhin Parmar== TEST FOUR EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertEquals(1, urinals.getFreeUrinals("10001"));
+    }
+
+    // Count max free urinals string 2
+    @Test
+    void getFreeUrinals2() {
+        System.out.println("====== Vidhin Parmar== TEST FIVE EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertEquals(0, urinals.getFreeUrinals("1001"));
+    }
+
+    // Count max free urinals string 3
+    @Test
+    void getFreeUrinals3() {
+        System.out.println("====== Vidhin Parmar== TEST SIX EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertEquals(3, urinals.getFreeUrinals("00000"));
+    }
+
+    // Count max free urinals string 4
+    @Test
+    void getFreeUrinals4() {
+        System.out.println("====== Vidhin Parmar== TEST SEVEN EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertEquals(2, urinals.getFreeUrinals("0000"));
+    }
+
+    // Count max free urinals string 5
+    @Test
+    void getFreeUrinals5() {
+        System.out.println("====== Vidhin Parmar== TEST EIGHT EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertEquals(1, urinals.getFreeUrinals("01000"));
+    }
+
+    // Count max free urinals string 6
+    @Test
+    void getFreeUrinals6() {
+        System.out.println("====== Vidhin Parmar== TEST NINE EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertEquals(-1, urinals.getFreeUrinals("011"));
+    }
+
+    // Good Case Public Urinal String
+    @Test
+    void goodPublicUrinalString() {
+        System.out.println("====== Vidhin Parmar== TEST TEN EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertTrue(urinals.goodPublicUrinalString("1010"));
+    }
+
+    // Bad Case Public Urinal String
+    @Test
+    void badPublicUrinalString() {
+        System.out.println("====== Vidhin Parmar== TEST ELEVEN EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertFalse(urinals.goodPublicUrinalString("1011"));
+    }
+
+    // Bad Case Public Urinal String
+    @Test
+    void badPublicUrinalString2() {
+        System.out.println("====== Vidhin Parmar== TEST TWELVE EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertFalse(urinals.goodPublicUrinalString("1012"));
+    }
+
+    // Bad Case Public Urinal String
+    @Test
+    void badPublicUrinalString3() {
+        System.out.println("====== Vidhin Parmar== TEST THIRTEEN EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertFalse(urinals.goodPublicUrinalString("101a"));
+    }
+
+    // Good Case Public Urinal String
+    @Test
+    void badPublicUrinalString4() {
+        System.out.println("====== Vidhin Parmar== TEST FOURTEEN EXECUTED =======");
+        Urinals urinals = new Urinals();
+        assertTrue(urinals.goodPublicUrinalString("101"));
     }
 }
